e4fa0a758c9ccc3ff130b2b3a81a0f637eace8b7
MPC-6Y83\Administrador <jose.sepulveda@sbd.com.co>
2021-02-02 23:23:28 -0500

    Creacion de la solucion y los proyectos (capas)

---
+++ CanteraSoft20x/CapaAccesoDatos/Conexion.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaAccesoDatos
+{
+    public class Conexion
+    {
+        public static string Conectar()
+        {
+            return ConfigurationManager.ConnectionStrings["cadena"].ConnectionString;
+        }
+    }
+}
